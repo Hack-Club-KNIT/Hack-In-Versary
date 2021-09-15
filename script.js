3a69386edc6128222f4008fbb3569d54cee6b27b
Ayush Singh <Ayush Singh>
2021-09-15 17:09:58 +0530

    Hack-In-Versary layout

---
+++ script.js
@@ -0,0 +1,9 @@
+function showdetails(){
+  document.getElementById("mainheader").style.display ="none"
+  document.getElementById("subheader").innerHTML ='<a href="/" style="text-decoration: none;font-size: 24px">◄ Back Home</a>'
+  document.getElementById("subheader").style.marginBottom ='14px'
+  document.getElementById("subheader").style.fontSize ='24px'
+  document.getElementById("text").innerHTML ='<h4 style="font-size: 40px; margin-bottom: 0; margin-top: -5px">Event <span style="font-size:40px; color:#ec3750">Details</span></h4> To be updated soon..' // event details
+  // document.getElementById("get-button").style.display ="none"
+  document.getElementById('tech-button').style.display ="none"
+}
