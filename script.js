3cca4a54c09002c3283bba4700ba951888b91af1
Ayush Singh <Ayush Singh>
2021-09-18 07:32:43 +0530

    Hack-In-Versary Schedule updated

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function showdetails(){
   document.getElementById("mainheader").style.display ="none"
-  document.getElementById("subheader").innerHTML ='<a href="/" style="text-decoration: none;font-size: 24px">◄ Back Home</a>'
+  document.getElementById("subheader").innerHTML ='<a href="/" style="text-decoration: none;font-size: 24px">◄  Back</a>'
   document.getElementById("subheader").style.marginBottom ='14px'
   document.getElementById("subheader").style.fontSize ='24px'
-  document.getElementById("text").innerHTML ='<h4 style="font-size: 40px; margin-bottom: 0; margin-top: -5px">Event <span style="font-size:40px; color:#ec3750">Details</span></h4> To be updated soon..' // event details
+  document.getElementById("text").innerHTML ='<h4 style="font-size: 40px; margin-bottom: 0; margin-top: -5px">Event <span style="font-size:40px; color:#ec3750">Details</span></h4> Event would be conducted on <button class="css-1rkl5kp"><a target="_blank" href="https://hack.af/z-join?id=cv8u1g">Hack Club Zoom at 7PM.</a></button><h3>Schedule</h3> <ul><li> Opening Ceremony </li> <li> Hacker Games </li><li> Hack Ex </li><li> Musical Nights & so much more</li></ul><b>!! Happy Hacking !!</b>' // event details
   // document.getElementById("get-button").style.display ="none"
   document.getElementById('tech-button').style.display ="none"
+  // document.getElementById("subfooter").innerHTML ='<a target="_blank" href="https://calendar.google.com/event?action=TEMPLATE&amp;tmeid=NmJoMTBvbHBhODRobjFoanVpaHBvMG83Y2cgYXl1c2guMTk0MThAa25pdC5hYy5pbg&amp;tmsrc=ayush.19418%40knit.ac.in">Add to Calender</a>'
 }
